837289d9ad450d53d796bfa7db84981c0335e0f4
mohanadbelal <41259916+mohanadbelal@users.noreply.github.com>
2020-05-02 14:32:37 +0200

    Add files via upload

--- api/src/main/java/api/IDController.java
+++ api/src/main/java/api/IDController.java
@@ -17,14 +17,16 @@ public class IDController {
     }
     @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping(path = "/Airport/getID/{ID}")
-    public String GetID(@PathVariable String ID) throws IOException {
-        Path walletPath = Paths.get("..","webapp","walletAirport");
+    public boolean GetID(@PathVariable String ID) throws IOException {
+        //Path walletPath = Paths.get("/tmp","wallet","walletAirport");
+        Path walletPath = Paths.get("/home","mohanad-belal",".fabric-vscode","environments","myEnv","wallets","Org1");
         Wallet wallet = Wallet.createFileSystemWallet(walletPath);
         // load a CCP
 
-        Path networkConfigPath = Paths.get("..","..","connection-profiles", "connection-airport.json");
+        Path networkConfigPath = Paths.get("/home","mohanad-belal",".fabric-vscode","environments","myEnv","gateways","Org1","Org1.json");
+        //Path networkConfigPath = Paths.get("/tmp","connection-profiles", "connection-airport.json");
         Gateway.Builder builder = Gateway.createBuilder();
-        builder.identity(wallet, "user1").networkConfig(networkConfigPath).discovery(true);
+        builder.identity(wallet, "org1Admin").networkConfig(networkConfigPath).discovery(true);
         try (Gateway gateway = builder.connect()) {
             // get the network and contract
             Network network = gateway.getNetwork("mychannel");
@@ -32,8 +34,41 @@ public class IDController {
             byte[] result;
 
             result = contract.evaluateTransaction("getID",ID);
+            //contract.submitTransaction("issueID", "003", "NN0", "Amira Nashaat Serry AbdelBar", "Female",
+              //     "Islam","SW", "Single","Egyption","1999-05-11");
+            return true;
+        } catch (ContractException e) {
+            e.printStackTrace();
+        } /*catch (InterruptedException e) {
+            e.printStackTrace();
+        } */
+
+        return false;
+    }
+
+    @CrossOrigin(origins = "http://localhost:4200")
+    @GetMapping(path = "/Airport/getInfo/{ID}")
+    public String GetInfo(@PathVariable String ID) throws IOException {
+        //Path walletPath = Paths.get("/tmp","wallet","walletAirport");
+        Path walletPath = Paths.get("/home","mohanad-belal",".fabric-vscode","environments","myEnv","wallets","Org1");
+        Wallet wallet = Wallet.createFileSystemWallet(walletPath);
+        // load a CCP
+
+        Path networkConfigPath = Paths.get("/home","mohanad-belal",".fabric-vscode","environments","myEnv","gateways","Org1","Org1.json");
+        //Path networkConfigPath = Paths.get("/tmp","connection-profiles", "connection-airport.json");
+        Gateway.Builder builder = Gateway.createBuilder();
+        builder.identity(wallet, "org1Admin").networkConfig(networkConfigPath).discovery(true);
+        try (Gateway gateway = builder.connect()) {
+            // get the network and contract
+            Network network = gateway.getNetwork("mychannel");
+            Contract contract = network.getContract("IDContract");
+            byte[] result;
+
+            result = contract.evaluateTransaction("getID",ID);
+            //contract.submitTransaction("issueID", "003", "NN0", "Amira Nashaat Serry AbdelBar", "Female",
+            //     "Islam","SW", "Single","Egyption","1999-05-11");
             System.out.println(new String(result));
-            return String.valueOf(result);
+            return new String(result);
         } catch (ContractException e) {
             e.printStackTrace();
         }
