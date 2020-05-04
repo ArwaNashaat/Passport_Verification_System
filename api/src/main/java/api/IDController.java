053dddc695dbba113678ba83f91040ee250d6652
ArwaNahsaat <47901895+arwanahsaat@users.noreply.github.com>
2020-05-04 16:45:40 +0200

    move ID & IDContractChaonCode from "chaincode" file to "api" file in "chaincodes package" + Renamed "IDContractChainCode" to "IDContract"

--- api/src/main/java/api/IDController.java
+++ api/src/main/java/api/IDController.java
@@ -1,14 +1,13 @@
 package api;
 
 import org.hyperledger.fabric.gateway.*;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.concurrent.TimeoutException;
 
 @RestController
 public class IDController {
@@ -32,16 +31,11 @@ public class IDController {
             Network network = gateway.getNetwork("mychannel");
             Contract contract = network.getContract("IDContract");
             byte[] result;
-
             result = contract.evaluateTransaction("getID",ID);
-            //contract.submitTransaction("issueID", "003", "NN0", "Amira Nashaat Serry AbdelBar", "Female",
-              //     "Islam","SW", "Single","Egyption","1999-05-11");
             return true;
         } catch (ContractException e) {
             e.printStackTrace();
-        } /*catch (InterruptedException e) {
-            e.printStackTrace();
-        } */
+        }
 
         return false;
     }
@@ -65,8 +59,6 @@ public class IDController {
             byte[] result;
 
             result = contract.evaluateTransaction("getID",ID);
-            //contract.submitTransaction("issueID", "003", "NN0", "Amira Nashaat Serry AbdelBar", "Female",
-            //     "Islam","SW", "Single","Egyption","1999-05-11");
             System.out.println(new String(result));
             return new String(result);
         } catch (ContractException e) {
@@ -74,6 +66,37 @@ public class IDController {
         }
 
         return "ID not found";
+    }
+
+    @CrossOrigin(origins = "http://localhost:4200")
+    @PostMapping(value="/Airport/IssueID", consumes= MediaType.APPLICATION_JSON_VALUE)
+    public boolean IssueID(@RequestBody ID ID) throws IOException {
+        //Path walletPath = Paths.get("/tmp","wallet","walletAirport");
+        Path walletPath = Paths.get("/home","mohanad-belal",".fabric-vscode","environments","myEnv","wallets","Org1");
+        Wallet wallet = Wallet.createFileSystemWallet(walletPath);
+        // load a CCP
+
+        Path networkConfigPath = Paths.get("/home","mohanad-belal",".fabric-vscode","environments","myEnv","gateways","Org1","Org1.json");
+        //Path networkConfigPath = Paths.get("/tmp","connection-profiles", "connection-airport.json");
+        Gateway.Builder builder = Gateway.createBuilder();
+        builder.identity(wallet, "org1Admin").networkConfig(networkConfigPath).discovery(true);
+        try (Gateway gateway = builder.connect()) {
+            // get the network and contract
+            Network network = gateway.getNetwork("mychannel");
+            Contract contract = network.getContract("IDContract");
+            byte[] result;
+            result = contract.submitTransaction("issueID", ID.getnumber(), ID.getAddress(), ID.getFullName(),
+                    ID.getGender(),ID.getReligion(),ID.getJob(), ID.getMaritalStatus(),ID.getNationality(),ID.getDateOfBirth());
+            return true;
+        } catch (ContractException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (TimeoutException e) {
+            e.printStackTrace();
+        }
+
+        return false;
     }
 
 }
