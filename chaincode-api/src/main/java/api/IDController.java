0ff4c25620989fbb98854d28442c3053bc027509
arwa nashaat <arwanashaat@gmail.com>
2020-05-06 05:09:34 +0200

    Renamed "id.getnumber()" to "id.getNumber()"

--- chaincode-api/src/main/java/api/IDController.java
+++ chaincode-api/src/main/java/api/IDController.java
@@ -44,17 +44,15 @@ public class IDController {
     @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping(path = "/Airport/getInfo/{ID}")
     public String GetInfo(@PathVariable String ID) throws IOException {
-        //Path walletPath = Paths.get("/tmp","wallet","walletAirport");
         Path walletPath = Paths.get("/home","arwa",".fabric-vscode","environments","airport","wallets","Org1");
         Wallet wallet = Wallet.createFileSystemWallet(walletPath);
-        // load a CCP
 
         Path networkConfigPath = Paths.get("/home","arwa",".fabric-vscode","environments","airport","gateways","Org1","Org1.json");
-        //Path networkConfigPath = Paths.get("/tmp","connection-profiles", "connection-airport.json");
+
         Gateway.Builder builder = Gateway.createBuilder();
         builder.identity(wallet, "org1Admin").networkConfig(networkConfigPath).discovery(true);
         try (Gateway gateway = builder.connect()) {
-            // get the network and contract
+
             Network network = gateway.getNetwork("mychannel");
             Contract contract = network.getContract("IDContract");
             byte[] result;
@@ -85,7 +83,7 @@ public class IDController {
             Network network = gateway.getNetwork("mychannel");
             Contract contract = network.getContract("IDContract");
             byte[] result;
-            result = contract.submitTransaction("issueID", id.getIDNumber(), id.getAddress(), id.getFullName(),
+            result = contract.submitTransaction("issueID", id.getNumber(), id.getAddress(), id.getFullName(),
                     id.getGender(),id.getReligion(),id.getJob(), id.getMaritalStatus(),id.getNationality(),id.getDateOfBirth());
             return true;
         } catch (ContractException e) {
