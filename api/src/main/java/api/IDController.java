6b95896a72306a9ef7dce9999c145bbe26ee1401
mohanadbelal <41259916+mohanadbelal@users.noreply.github.com>
2020-05-16 18:03:08 +0200

    Certificate requests

--- api/src/main/java/api/IDController.java
+++ api/src/main/java/api/IDController.java
@@ -1,5 +1,6 @@
 package api;
 
+import chaincodes.Certificate;
 import org.hyperledger.fabric.gateway.*;
 import chaincodes.ID;
 
@@ -17,37 +18,14 @@ public class IDController {
         System.setProperty("org.hyperledger.fabric.sdk.service_discovery.as_localhost", "true");
     }
     @CrossOrigin(origins = "http://localhost:4200")
-    @GetMapping(path = "/Airport/getID/{ID}")
-    public boolean GetID(@PathVariable String ID) throws IOException {
-        Path walletPath = Paths.get("/home","arwa",".fabric-vscode","environments","airport","wallets","Org1");
-        Wallet wallet = Wallet.createFileSystemWallet(walletPath);
-        String[] strings = {"5","l"};
-
-        Path networkConfigPath = Paths.get("/home","arwa",".fabric-vscode","environments","airport","gateways","Org1","Org1.json");
-
-        Gateway.Builder builder = Gateway.createBuilder();
-        builder.identity(wallet, "org1Admin").networkConfig(networkConfigPath).discovery(true);
-        try (Gateway gateway = builder.connect()) {
-
-            Network network = gateway.getNetwork("mychannel");
-            Contract contract = network.getContract("IDContract");
-            byte[] result;
-            result = contract.evaluateTransaction("getID",ID);
-            return true;
-        } catch (ContractException e) {
-            e.printStackTrace();
-        }
-
-        return false;
-    }
-
-    @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping(path = "/Airport/getInfo/{ID}")
     public String GetInfo(@PathVariable String ID) throws IOException {
-        Path walletPath = Paths.get("/home","arwa",".fabric-vscode","environments","airport","wallets","Org1");
-        Wallet wallet = Wallet.createFileSystemWallet(walletPath);
+        Path walletPath = Paths.get("/home","mohanad-belal",".fabric-vscode","environments","myEnv","wallets","Org1");
 
-        Path networkConfigPath = Paths.get("/home","arwa",".fabric-vscode","environments","airport","gateways","Org1","Org1.json");
+        Wallet wallet = Wallet.createFileSystemWallet(walletPath);
+        // load a CCP
+
+        Path networkConfigPath = Paths.get("/home","mohanad-belal",".fabric-vscode","environments","myEnv","gateways","Org1","Org1.json");
 
         Gateway.Builder builder = Gateway.createBuilder();
         builder.identity(wallet, "org1Admin").networkConfig(networkConfigPath).discovery(true);
@@ -70,11 +48,12 @@ public class IDController {
     @CrossOrigin(origins = "http://localhost:4200")
     @PostMapping(value="/Airport/IssueID", consumes= MediaType.APPLICATION_JSON_VALUE)
     public boolean IssueID(@RequestBody ID id) throws IOException {
+        Path walletPath = Paths.get("/home","mohanad-belal",".fabric-vscode","environments","myEnv","wallets","Org1");
 
-        Path walletPath = Paths.get("/home","arwa",".fabric-vscode","environments","airport","wallets","Org1");
         Wallet wallet = Wallet.createFileSystemWallet(walletPath);
+        // load a CCP
 
-        Path networkConfigPath = Paths.get("/home","arwa",".fabric-vscode","environments","airport","gateways","Org1","Org1.json");
+        Path networkConfigPath = Paths.get("/home","mohanad-belal",".fabric-vscode","environments","myEnv","gateways","Org1","Org1.json");
 
         Gateway.Builder builder = Gateway.createBuilder();
         builder.identity(wallet, "org1Admin").networkConfig(networkConfigPath).discovery(true);
@@ -96,6 +75,72 @@ public class IDController {
 
         return false;
     }
+
+
+    @CrossOrigin(origins = "http://localhost:4200")
+    @GetMapping(path = "/Hospital/getCertificate/{ID}")
+    public String GetCertificate(@PathVariable String ID) throws IOException {
+        Path walletPath = Paths.get("/home","mohanad-belal",".fabric-vscode","environments","myEnv","wallets","Org1");
+
+        Wallet wallet = Wallet.createFileSystemWallet(walletPath);
+        // load a CCP
+
+        Path networkConfigPath = Paths.get("/home","mohanad-belal",".fabric-vscode","environments","myEnv","gateways","Org1","Org1.json");
+
+        Gateway.Builder builder = Gateway.createBuilder();
+        builder.identity(wallet, "org1Admin").networkConfig(networkConfigPath).discovery(true);
+        try (Gateway gateway = builder.connect()) {
+
+            Network network = gateway.getNetwork("mychannel");
+            Contract contract = network.getContract("CertificateContract");
+            byte[] result;
+
+            result = contract.evaluateTransaction("getCertificate",ID);
+            System.out.println(new String(result));
+            return new String(result);
+        } catch (ContractException e) {
+            e.printStackTrace();
+        }
+
+        return "ID not found";
+    }
+
+
+    @CrossOrigin(origins = "http://localhost:4200")
+    @PostMapping(value="/Hospital/issueCertificate", consumes= MediaType.APPLICATION_JSON_VALUE)
+    public boolean issueCertificate(@RequestBody Certificate ct) throws IOException {
+        Path walletPath = Paths.get("/home","mohanad-belal",".fabric-vscode","environments","myEnv","wallets","Org1");
+
+        Wallet wallet = Wallet.createFileSystemWallet(walletPath);
+        // load a CCP
+
+        Path networkConfigPath = Paths.get("/home","mohanad-belal",".fabric-vscode","environments","myEnv","gateways","Org1","Org1.json");
+
+        Gateway.Builder builder = Gateway.createBuilder();
+        builder.identity(wallet, "org1Admin").networkConfig(networkConfigPath).discovery(true);
+        try (Gateway gateway = builder.connect()) {
+
+            Network network = gateway.getNetwork("mychannel");
+            Contract contract = network.getContract("CertificateContract");
+            byte[] result;
+            result = contract.submitTransaction("issueCertificate",ct.getNumber(),ct.getBirthPlace(),ct.getFullName(),
+                    ct.getGender(),ct.getReligion(),ct.getNationality(),ct.getDateOfBirth(),ct.getFatherName(),ct.getFatherReligion(),
+                    ct.getFatherNationality(),ct.getMotherName(),ct.getMotherReligion(),ct.getMotherNationality());
+            return true;
+        } catch (ContractException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (TimeoutException e) {
+            e.printStackTrace();
+        }
+
+        return false;
+    }
+
+
+
+
 
 }
 
